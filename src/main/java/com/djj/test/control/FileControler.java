77ae660720503608fe9075c4eee436358d7b1fe2
djj1111 <dengjiajun1108@gmail.com>
2017-02-07 16:31:26 +0800

    4

--- src/main/java/com/djj/test/control/FileControler.java
+++ src/main/java/com/djj/test/control/FileControler.java
@@ -3,6 +3,7 @@ package com.djj.test.control;
 import com.djj.test.entity.File;
 import com.djj.test.service.FileService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,6 +27,16 @@ public class FileControler {
 
         // request.setAttribute("userList", userManager.getAllUser());
 
+        return fileService.getAllFile();
+    }
+
+    @ResponseBody
+    @RequestMapping("/savefile/{path}")
+    public List<File> saveFile(@PathVariable String path) {
+        File file = new File();
+        file.setMid(9);
+        file.setPath(path);
+        fileService.addFile(file);
         return fileService.getAllFile();
     }
 }
